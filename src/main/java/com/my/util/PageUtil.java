d61fcd0dc9e73a66fd500c3af856eaf76df2c1cf
zhangKun <zhangKunCode@qq.com>
2024-07-23 12:53:13 +0800

    提交二次查询法

--- src/main/java/com/my/util/PageUtil.java
+++ src/main/java/com/my/util/PageUtil.java
@@ -5,8 +5,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -16,7 +15,7 @@ public class PageUtil {
 
 
     // 方法一： 全局视野法
-    public static <T> PageInfo<T> multiPageInfo1(List<Supplier<List<T>>> supplierList, int pageNo, int pageSize, Comparator<T> comparator) throws Exception {
+    public static <T> PageInfo<T> multiPageInfo1(ArrayList<Supplier<List<T>>> supplierList, int pageNo, int pageSize, Comparator<T> comparator) throws Exception {
         if (CollectionUtil.isEmpty(supplierList)) {
             throw new Exception("异常");
         }
@@ -37,5 +36,46 @@ public class PageUtil {
         pageInfo.setTotal(total);
         pageInfo.setPages((total - 1) / pageSize + 1);
         return pageInfo;
+    }
+
+    // 二次查询法
+    public static <T> HashMap<String, List<T>> multiPageInfoMap(ArrayList<Supplier<List<T>>> supplierList, int pageNo, int pageSize, Comparator<T> comparator) throws Exception {
+        int offset = (pageNo - 1) * pageSize;
+        int n = supplierList.size();
+        int offsetSplit = offset / n;
+        List<T> maxValue = new ArrayList<>();
+        List<T> minValue = new ArrayList<>();
+        Optional<T> totalMin = supplierList.stream().map(supplier -> {
+            PageHelper.offsetPage(offsetSplit, pageSize, true);
+            List<T> tempList = supplier.get();
+            Optional<T> max = tempList.stream().max(comparator);
+            if (max.isPresent()) {
+                maxValue.add(max.get());
+            }
+
+            return tempList;
+        }).flatMap(List::stream).min(comparator);
+        if (totalMin.isPresent()) {
+            minValue.add(totalMin.get());
+        }
+        HashMap<String, List<T>> resultMap = new HashMap<>();
+        resultMap.put("maxValue",maxValue);
+        resultMap.put("minValue",minValue);
+        return resultMap;
+    }
+
+    public static <T>  PageInfo<T> multiPageInfo2(ArrayList<Supplier<List<T>>> supplierList, int pageNo, int pageSize, Comparator<T> comparator) throws Exception {
+        int n = supplierList.size();
+        int totalCount = supplierList.stream()
+                .mapToInt(supplier -> supplier.get().size()) // 获取每个Supplier提供的列表并转换为int
+                .sum();
+        int offset = ((pageNo - 1) * pageSize) / n * n - (totalCount - n);
+        List<T> resAllList = supplierList.stream().map(supplier -> {
+            PageHelper.startPage(pageNo, pageSize, true);
+            return supplier.get();
+        }).flatMap(List::stream).collect(Collectors.toList());
+        List<T> resList = resAllList.stream().sorted(comparator).skip((pageNo - 1) * pageSize - offset)
+                .limit(pageSize).collect(Collectors.toList());
+        return getPageInfo(pageNo, pageSize, resAllList.size(), resList);
     }
 }
---
+++ src/test/java/com/my/dao/TestPageMapper.java
@@ -0,0 +1,48 @@
+package com.my.dao;
+
+
+import com.github.pagehelper.PageInfo;
+import com.my.dao.master.UserMapperMaster;
+import com.my.dao.slaver.UserMapperSlaver;
+import com.my.entity.User;
+import com.my.util.PageUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Supplier;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestPageMapper {
+
+    @Autowired
+    private UserMapperMaster userMapperMaster;
+
+    @Autowired
+    private UserMapperSlaver userMapperSlaver;
+
+    @Test
+    public void test() throws Exception {
+        ArrayList<Supplier<List<User>>> supplierList = new ArrayList<>();
+        supplierList.add(() -> userMapperMaster.selectAll());
+        supplierList.add(() -> userMapperSlaver.selectAll());
+        PageInfo<User> pageInfo = PageUtil.multiPageInfo1(supplierList, 0, 2, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getUsername().compareTo(o2.getUsername());
+            }
+        });
+        List<User> list = pageInfo.getList();
+        list.stream().forEach(
+                user -> System.out.printf(user.getUsername())
+        );
+        System.out.println("end");
+    }
+
+}
